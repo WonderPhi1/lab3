9e17b5519b0d4c4d91cf84177441a85d679a49f2
virs2729 <virs2729@mailhost.cs.uleth.ca~>
2022-10-14 19:45:08 -0600

    Comments and doxygen added for Person and Staff

---
+++ doxygen/html/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['staff_0',['Staff',['../class_staff.html',1,'Staff'],['../class_staff.html#afdf168fd8f0269bb65c0aa1cdac3093c',1,'Staff::Staff()']]],
+  ['student_1',['Student',['../class_student.html',1,'']]]
+];
